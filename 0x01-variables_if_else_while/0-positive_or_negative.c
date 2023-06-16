c1c81c01fd9ebbdbda87518b9697422488f7acc8
Demidorn <dornkainzen@gmail.com>
2023-06-16 08:58:28 -0700

    assign random variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -23,11 +23,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf(" is zero\n");
 	}
 	else
 	{
-		printf("is negative\n");
+		printf(" is negative\n");
 	}
 	return (0);
 }
