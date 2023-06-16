0fcfa17475c736c7058b9c17a7fa8c60e0a66802
Demidorn <dornkainzen@gmail.com>
2023-06-16 11:20:33 -0700

    prints out last digit of variable and it's argument

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,27 +10,24 @@
 
 int main(void)
 {
-	int n, z;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
 
-	z = n % 10;
-	printf(" %d \n", z);
-
-	if (n > 5)
+	if (n % 10 > 5)
 	{
-		printf("Last digit of" % d "is" % d "and is greater than 5 \n", n, z);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
-	else if (n == 0)
+	else if (n % 10 == 0)
 	{
-		printf(" Last digit of" % d "is" % d "and is 0 \n", n, z);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of" % d "is" % d "and is less than 6 and not 0 \n", n, z);
-
+		printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, n % 10);
+	}
 	return (0);
 }
