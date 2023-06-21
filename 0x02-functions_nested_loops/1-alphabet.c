0e41de1c6f625599ee6bb2e59b4063a466406423
Demidorn <dornkainzen@gmail.com>
2023-06-21 00:48:35 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ *
+ ** Return: void
+ */
+void print_alphabet(void)
+{
+	char ness = 'a';
+	while(ness <= 'z')
+	{
+		_putchar(ness);
+		ness++;
+	}
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+	int s;
+	for (j = 1; j <= 10; j++)
+	{
+		for(s = 97; s <= 122; s++)	
+		_putchar(j);
+	}
+	_putchar('\n');
+	
+}
