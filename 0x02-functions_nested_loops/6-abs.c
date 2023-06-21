cc7b2407fa69edac877214b7e4e682dd156ddfe9
Demidorn <dornkainzen@gmail.com>
2023-06-21 16:02:09 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Prints absolute value of an integer
+ *
+ * Return: Always 0 (success)
+ */
+
+int _abs(int)
+{
+	int x = -6, y;
+
+	y = abs(-6);
+
+	_putchar("The absolute value of %d.\n", y);
+
+	return (0);
+
+}
