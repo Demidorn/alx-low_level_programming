2ab30262bc2c90c5fd77b0bae811b95b4af2a742
Demidorn <dornkainzen@gmail.com>
2023-06-22 16:47:47 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print the numbers since 0 up to 9
+ * Description: Prints the numbers excluding 2 and 4
+ * Return: The numbers since 0 up to 9
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+	
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x);
+		}
+	}
+	_putchar('\n');
+}
