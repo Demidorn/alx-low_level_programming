5f4e33464693f62c720acaafd8e62d4b49452ceb
Demidorn <dornkainzen@gmail.com>
2023-07-03 15:48:25 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,23 +9,23 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, n;
-	int count = 0;
+	unsigned int i, n, count, zex;
+
+	count = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-	int zex = 0;
-
-	for (n = 0; accept[n] != '\0'; n++)
-	{
-		if (accept[n] == s[i])
+		zex = 0;
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			count++;
-			zex = 1;
-		}
-		if (zex  == 0)
-		return	(count);
-	}
+			if (accept[n] == s[i])
+			{
+				count++;
+				zex = 1;
+			}
+			}
+		if (zex == 0)
+			return (count);
 	}
 	return (count);
 }
