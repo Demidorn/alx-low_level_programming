bb96b5a048057ec787d0b025cb98a727edcef4b6
Demidorn <dornkainzen@gmail.com>
2023-07-05 06:36:19 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: String to be checked
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
