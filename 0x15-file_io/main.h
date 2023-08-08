bed21144e2ee5b3f432df210b7535fecc40b26dc
Demidorn <dornkainzen@gmail.com>
2023-08-08 01:49:52 -0700

    header files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,13 @@
-#ifndef MAIN_H_
-#define MAINA_H_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
