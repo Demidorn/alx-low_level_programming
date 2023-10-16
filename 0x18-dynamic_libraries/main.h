b82e5a1de04fcb8be2ec239dd01a7574b7258808
Demidorn <dornkaizen@gmail.com>
2023-10-16 04:26:18 -0700

    header files and prototypes

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -12,7 +12,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -26,5 +26,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
-
+#endif /*MAIN_H*/
