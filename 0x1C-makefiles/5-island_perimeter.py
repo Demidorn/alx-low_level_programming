c2182a548963a5be734e474089def72da49c3c05
Demidorn <dornkaizen@gmail.com>
2023-12-28 01:18:04 -0800

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" a function that defines perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """
+    Returns perimeter of an island
+    Grid representation:
+            0 represents a water zone
+            1 represents a land zone
+    Returns: 
+            perimeter of an island defined in grid
+    """
+
+    width = len(gird[0])
+    length = len(grid)
+    edges = 0
+    size = 0 
+
+    for i in range(height):
+        for j in  range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
